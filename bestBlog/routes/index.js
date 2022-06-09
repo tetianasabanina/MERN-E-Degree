8e9d903a99ea93ff69e68bc8a3b8e0584f82ba6c
tetianasabanina <tatiana08st@gmail.com>
2022-06-09 20:25:48 +0300

    Add application routes

--- bestBlog/routes/index.js
+++ bestBlog/routes/index.js
@@ -1,9 +1,30 @@
 var express = require('express');
 var router = express.Router();
+var TopicController = require('../controllers/topic.controller')
+var UserController = require('../controllers/user.controller')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'My Blog' });
 });
+
+/* GET */
+router.get('/user', UserController.getAllUsers)
+router.get('/topic', TopicController.getAllTopics)
+router.get('/topic/type/:topicType', TopicController.getTopicByType)
+router.get('/topic/notprivate/', TopicController.getTopicByNotPrivate)
+router.get('/topic/tags/', TopicController.getTopicByTags)
+router.get('/topic/:id', TopicController.getTopicById)
+
+/* POST */
+router.post('/topic/new', TopicController.createNewTopic)
+router.post('/user/new', UserController.createNewUser)
+
+/* PUT */
+router.put('/topic/tags/', TopicController.updateTopicTags)
+router.put('/topic/', TopicController.updateTopicProperty)
+
+/* Delete */
+router.delete('/topic/:id', TopicController.removeTopic)
 
 module.exports = router;
