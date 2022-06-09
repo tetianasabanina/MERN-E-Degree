a3ea2b18fde00558f14d0352c23455ca45191fbf
tetianasabanina <tatiana08st@gmail.com>
2022-06-09 20:24:23 +0300

    Update db connection

--- bestBlog/db.js
+++ bestBlog/db.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose')
 mongoose.Promise = require('bluebird')
 
-// mongoose.connect('mongodb+srv://testuser:test@bestBlog.mahho.mongodb.net/?retryWrites=true&w=majority')
-// mongoose.connect('mongodb://127.0.0.1.27017/bestBlog')
-// mongoose.connect('mongodb://localhost:27017/bestBlog')
-mongoose.connect('mongodb+srv://tatiana08:tatiana08@cluster0.znxsc.mongodb.net/?retryWrites=true&w=majority')
+// mongoose.connect('mongodb+srv://testuser:test@bestblog.mahho.mongodb.net/?retryWrites=true&w=majority')
+// mongoose.connect('mongodb://127.0.0.1.27017/bestblog')
+// mongoose.connect('mongodb://localhost:27017/bestblog')
+mongoose.connect('mongodb+srv://testuser:test@snacktime.mahho.mongodb.net/?retryWrites=true&w=majority')
   .then(() => {
     console.log('success')
   }).catch(() => {
